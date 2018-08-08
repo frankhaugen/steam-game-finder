bc6d9dcaa7fe158509be70bc752baada96e1cfb1
frank <frank.haugen@gmail.com>
2018-08-08 05:36:17 +0200

    It works

--- steam-game-finder/MainWindow.xaml.cs
+++ steam-game-finder/MainWindow.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-using System.IO;
+
+//using System.Net.Http;
+
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace steam_game_finder
 {
@@ -23,15 +19,32 @@ namespace steam_game_finder
     public partial class MainWindow : Window
     {
         public DataGrid GamesDetails = new DataGrid();
+        //public List<string> GamesList = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        public void CollectGamesList()
-        {
+            WebClient webClient = new WebClient();
+            string json = webClient.DownloadString("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY2&format=json");
 
+
+            JObject jsonObject = JObject.Parse(json);
+            JArray jsonArray = (JArray)jsonObject["applist"]["apps"];
+
+            string[] gameNames = jsonArray.Select(app => app["name"]).Values<string>().ToArray<string>();
+            int[] gameIDs = jsonArray.Select(app => app["appid"]).Values<int>().ToArray();
+
+
+
+            Dictionary<int, string> GamesList = new Dictionary<int, string>();
+
+            for (int i = 0; i < gameIDs.Count(); i++)
+            {
+                GamesList.Add(gameIDs[i], gameNames[i]);
+            }
+
+            //TextField.Text = String.Join("\n", strLst.ToArray<string>());
         }
     }
 }
